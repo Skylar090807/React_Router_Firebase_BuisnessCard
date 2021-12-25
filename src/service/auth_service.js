e14b9662e732bc132071956de0ff73080fe0e9dc
Skylar <skylar090807@gmail.com>
2021-12-25 21:04:28 +0900

    Add Firebase

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,1 +1,37 @@
-import firebase from 'firebase'
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth'
+
+class AuthService {
+  constructor() {
+    this.firebaseAuth = getAuth()
+    this.googleProvider = new GoogleAuthProvider()
+    this.githubProvider = new GithubAuthProvider()
+  }
+
+  login(providerName) {
+    const authProvider = this.getProvider(providerName)
+    return signInWithPopup(this.firebaseAuth, authProvider)
+  }
+
+  logout() {
+    this.firebaseAuth.signOut()
+  }
+
+  onAuthChange(onUserChanged) {
+    this.firebaseAuth.onAuthStateChanged((user) => {
+      onUserChanged(user)
+    })
+  }
+
+  getProvider(providerName) {
+    switch (providerName) {
+      case 'Google':
+        return this.googleProvider
+      case 'Github':
+        return this.githubProvider
+      default:
+        throw new Error(`not supported provider: ${providerName}`)
+    }
+  }
+}
+
+export default AuthService
